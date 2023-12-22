778f83486f416f0889547db01ba0f30eb0a9b8df
Abdulmajid Awol <111248496+maajidAwol@users.noreply.github.com>
2023-12-23 00:27:32 +0300

    Added solution for Rotate Image

---
+++ week1/rotate-image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+    
+        for r in range(len(matrix)):
+            for c in range(r,len(matrix[0])):  
+                matrix[r][c],matrix[c][r] = matrix[c][r],matrix[r][c]
+        for i in range(len(matrix)):
+            matrix[i].reverse()
+        
+        
