acde4de032750ec3443e19157141c514be5e29c6
Abdulmajid Awol <111248496+maajidAwol@users.noreply.github.com>
2024-01-03 18:57:13 +0300

    Added solution for Minimum Common Value

---
+++ week1/minimum-common-value.py
@@ -0,0 +1,6 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        if set(nums1) & set(nums2):
+            return min(set(nums1) & set(nums2))
+        else:
+            return -1
