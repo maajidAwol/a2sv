ace145a249f8e877046beb96b690940d66fc552f
Abdulmajid Awol <111248496+maajidAwol@users.noreply.github.com>
2024-01-05 19:57:57 +0300

    Added solution for Boats to Save People

--- week1/boats-to-save-people.py
+++ week1/boats-to-save-people.py
@@ -1,20 +1,15 @@
-class Solution(object):
-    def numRescueBoats(self, people, limit):
-        """
-        :type people: List[int]
-        :type limit: int
-        :rtype: int
-        """
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
-        res=0
-        left = 0
-        right = len(people)-1
-        while left <= right:
-            if people[left] + people[right] <= limit:
-                left +=1
-                right -=1
-                res +=1
-            elif people[left] + people[right] > limit:
-                right -=1
-                res +=1
-        return res
+        boats = 0
+        l = 0
+        r =len(people)-1
+        while l <= r:
+            if people[l] +people[r] <= limit:
+                l +=1
+                r -=1
+                boats+=1
+            else:
+                r -=1
+                boats +=1
+        return boats
