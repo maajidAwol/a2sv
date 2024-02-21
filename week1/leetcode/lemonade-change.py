7c6303235fc9e765ba06353ba799f1a8785b0e23
Abdulmajid Awol <111248496+maajidAwol@users.noreply.github.com>
2024-02-21 10:28:36 +0300

    Add solution for Lemonade Change

--- week1/leetcode/lemonade-change.py
+++ week1/leetcode/lemonade-change.py
@@ -1,17 +1,14 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        count = {5:0,10:0,20:0}
-        for x in bills:
-            if x ==5:
-                count[5]+=1
-            elif x ==10:
-                count[10] +=1
+        count = defaultdict(int)
+        for x in bills: 
+            count[x] +=1
+            if x ==10:
                 if count[5]:
                     count[5] -=1
                 else:
                     return False
-            else:
-                count[20] +=1
+            elif x==20:
                 if count[5] and count[10]:
                     count[5] -=1
                     count[10] -=1
