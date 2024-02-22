d8a675d448b07735c8250e63b6852581eca9fad8
Abdulmajid Awol <111248496+maajidAwol@users.noreply.github.com>
2024-02-22 11:57:09 +0300

    Add solution for Number of Recent Calls

--- week1/leetcode/number-of-recent-calls.py
+++ week1/leetcode/number-of-recent-calls.py
@@ -2,13 +2,11 @@ class RecentCounter:
 
     def __init__(self):
         self.count = []
+        self.cnt = 0
     def ping(self, t: int) -> int:
-        if self.count:
-            while self.count and self.count[0] < (t-3000):
-                self.count = self.count[1:]
-            self.count.append(t)
-        else:
-            self.count.append(t)
+        self.count.append(t)
+        while self.count and self.count[0] < (t-3000):
+            self.count = self.count[1:]
         return len(self.count)
 
 # Your RecentCounter object will be instantiated and called as such:
