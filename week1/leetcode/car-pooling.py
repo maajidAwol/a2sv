f074e0e5d059d84dd52365d60efa40602925ffbb
Abdulmajid Awol <111248496+maajidAwol@users.noreply.github.com>
2024-03-07 23:06:59 +0300

    Add solution for Car Pooling

--- week1/leetcode/car-pooling.py
+++ week1/leetcode/car-pooling.py
@@ -1,11 +1,24 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        ans = [0]*1001
+        max_ = max(trips,key = lambda x: x[2])[2]
+        ans = [0]*(max_+2)
         for i in range(len(trips)):
-            ans[trips[i][1]] +=trips[i][0]
-            ans[trips[i][2]] -= trips[i][0]
-        for j in range(len(ans)):
+            ans[trips[i][1]+1] +=trips[i][0]
+            ans[trips[i][2]+1] -= trips[i][0]
+        for j in range(1,len(ans)):
             ans[j] = ans[j-1] +ans[j]
             if ans[j] >capacity:
                 return False
         return True
+
+# class Solution:
+#     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+#         ans = [0]*1001
+#         for i in range(len(trips)):
+#             ans[trips[i][1]] +=trips[i][0]
+#             ans[trips[i][2]] -= trips[i][0]
+#         for j in range(len(ans)):
+#             ans[j] = ans[j-1] +ans[j]
+#             if ans[j] >capacity:
+#                 return False
+#         return True
